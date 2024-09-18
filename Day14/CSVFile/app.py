c47b21a5d1dea8ee8684aae57ea8bc73855c6359
dai <dai@students.acts.in>
2024-09-18 09:55:00 +0530

    17/09/2024

---
+++ Day14/CSVFile/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import pandas as pd
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    file = None
+    if request.method == 'POST':
+        file = pd.read_csv(request.form['file'])
+    return render_template('home.html', file=file)
+
+if __name__ == '__main__':
+    app.run(debug=True)
